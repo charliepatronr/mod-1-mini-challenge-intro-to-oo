067e4bf6e48b5d0a062a4909022d92ca2ee6b8ef
charliepatronr <54086538+charliepatronr@users.noreply.github.com>
2020-08-07 00:06:17 -0500

    Done

--- models/clown.rb
+++ models/clown.rb
@@ -1,4 +1,59 @@
 class Clown
-  # write methods here!
 
+  attr_accessor :name, :skill, :fears, :age 
+
+  @@all = []
+
+  def initialize (name, age, skill, fears)
+    @name = name
+    @age = age
+    @skill = skill
+    @fears = fears
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def lie_about_age (new_age)
+    self.age = new_age
+  end
+  
+  def overcome_fears
+    self.fears = nil
+  end
+
+  def say_hi
+    "Hello my name is #{name}. I'm #{age} years old. I'm good at #{skill} and #{fears}"
+  end
+
+  def self.names
+    self.all.collect do |individual_clown|
+      individual_clown.name
+    end
+  end 
+
+  def self.find_by_name(name)
+    self.all.find{|clown| clown.name == name}
+  end
+
+  def self.oldest
+    comparison_age = 0
+    oldest_name = ''
+    self.all.each do |individual_clown|
+      if(individual_clown.age > comparison_age)
+        comparison_age = individual_clown.age
+        oldest_name = individual_clown.name
+      end
+    end
+    self.all.find{|clown| clown.name == oldest_name}
+  end 
+
+  def self.fearless
+    self.all.select do |individual_clown|
+      individual_clown.fears == nil
+    end
+  end 
+  
 end # end of Clown class
--- run.rb
+++ run.rb
@@ -1,20 +1,31 @@
 require 'pry'
 require_relative "./models/clown"
 
-# test your code here!
-# create a few new clown instances, for example:
-# krusty = Clown.new("Krusty", 55, "Juggling Chainsaws", "Failure")
+krusty = Clown.new("Krusty", 55, "Juggling Chainsaws", "Failure")
+fizbo = Clown.new("Fizbo", 44, "Kids birthday parties", " I'm scared of loosing lily")
 
-# then run "ruby run.rb" and call your methods to make sure your code works
-# for example:
+# Instance methods tests to run
+
 # krusty.name 
-# => "Krusty"
 # krusty.name = "Krusty the Krustacular"
 # krusty.name 
-# => "Krusty the Krustacular"
 # krusty.fears = "Children"
-# krusty.fears
-# => "Failure"
+# krusty.overcome_fears
+# fizbo.say_hi
+# fizbo.name
+# fizbo.fears
+# fizbo.age
+# fizbo.lie_about_age(70)
+# fizbo.age
+
+# Class method tests
+clown_names = Clown.names
+fearless_clowns1 = Clown.fearless
+find_clown_by_name = Clown.find_by_name ("Fizbo")
+oldest_clown = Clown.oldest
+fizbo.overcome_fears
+fearless_clowns2 = Clown.fearless
+
 
 binding.pry
 "pls"
